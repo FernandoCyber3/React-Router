aff3637bc4cd0e2661b24c1f7cb26cd1cb7c83d2
Fernando <fernando.h.357@outlook.com>
2026-04-05 21:28:36 -0300

    React Router

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.css"
+
+import {Outlet} from "react-router"
+
+export function Layout(){
+    return(
+    <div>
+        <header className="user">
+            <p>Olá Fernando</p>
+        </header>
+
+        <Outlet />
+
+
+        <footer>
+            <span>Todos os direitos reservados </span>
+        </footer>
+    </div>
+    )
+}
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route} from "react-router"
+
+import {Layout} from "../components/Layout"
+import { Home } from "../pages/home"
+import { Products} from "../pages/Products"
+import { NotFound } from "../pages/NotFound"
+import { Details } from "../pages/Details"
+
+export function AppRoutes(){
+    return (
+        <Routes>
+
+           <Route path="/" element={<Layout />}>
+            <Route path="/" index element={<Home/>} />
+            <Route path="/products" element={<Products/>} />
+            
+        </Route>
+        
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    )
+}
